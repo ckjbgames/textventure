1d67bd3abfde649bf165b1755da45e679db09631
ckjbgames <ckjbgames@gmail.com>
2017-01-10 17:28:51 -0500

    Update launcher.sh

--- launcher/launcher.sh
+++ launcher/launcher.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 # textventure-launcher.sh
-prompt1="(L) Log in\n\n(Q) Quit\n\n\n\n"
-choices1='
-prompt2="(P) Play textventure\n\n(Q) Quit\n\n\n\n"
-gamepath="/var/games/textventure.py" # This is an example
-echo "$prompt1"
-read choice
-reset
-case ${choice in
-[L|l]*)
-  login()
-  ;;
-[Q|q
+prompt1='(L) Log In\n\n(Q) Quit\n\n(H) Help\n\n\n'
+prompt2='(P) Play Textventure\n\n(Q) Quit\n\n(H) Help\n\n\n'
+login1='Please enter your username (email address). Abort with a blank entry.\n'
+login2='Please enter your password. Abort with a blank entry.\n'
+login() {
+    echo "$login1"
+    read username
+    reset
+    if [$username -ne '']; then
+        echo "$login2"
+        if [$
