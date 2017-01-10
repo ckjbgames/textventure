17cd8b8ba31ee540be44d64d29516478889aaa6c
ckjbgames <ckjbgames@gmail.com>
2017-01-09 20:39:22 -0500

    Update launcher.sh

--- launcher/launcher.sh
+++ launcher/launcher.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 # textventure-launcher.sh
-prompt1="(L) Log in\n(Q) Quit"
-prompt2="(P) Play textventure\n(Q) Quit"
+prompt1="(L) Log in\n\n(Q) Quit\n\n\n\n"
+choices1='
+prompt2="(P) Play textventure\n\n(Q) Quit\n\n\n\n"
 gamepath="/var/games/textventure.py" # This is an example
 echo "$prompt1"
+read choice
+reset
+case ${choice in
+[L|l]*)
+  login()
+  ;;
+[Q|q
