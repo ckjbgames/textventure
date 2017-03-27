46e7a101dc40e8684358986aeda933262899bc25
ckjbgames <ckjbgames@gmail.com>
2017-03-27 12:46:01 -0400

    Update launcher.bat

--- launcher/launcher.bat
+++ launcher/launcher.bat
@@ -1,10 +1,14 @@
 @ECHO OFF
-SETLOCAL
-rem textenture launcher
+SETLOCAL EnableDelayedExpansion
+REM textenture launcher
 :select
+    REM Equivalent to bash/ksh "select."
     SET count=1
-    rem Equivalent to bash/ksh "select."
     FOR /F %%option in ('echo %*') DO (
         ECHO %count%) %%option
         SET /A count+=1
     )
+    SET /P choice="Enter Choice: "
+EXIT /B %choice%
+:menu1
+    SET options=Login Help Quit
