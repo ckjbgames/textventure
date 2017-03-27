335a9b76d323118e479a5b4d60c2e5c6b323d3ae
ckjbgames <ckjbgames@gmail.com>
2017-03-27 11:38:16 -0400

    Update launcher.bat

--- launcher/launcher.bat
+++ launcher/launcher.bat
@@ -1,1 +1,10 @@
-rem textventure launcher
+@ECHO OFF
+SETLOCAL
+rem textenture launcher
+:select
+    SET count=1
+    rem Equivalent to bash/ksh "select."
+    FOR /F %%option in ('echo %*') DO (
+        ECHO %count%) %%option
+        SET /A count+=1
+    )
